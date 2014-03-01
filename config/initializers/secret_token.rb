aba415f710d2fbc90afa7f0f3361e508284b793a
calvarado <calvarado@wesleyan.edu>
2014-03-01 11:30:07 -0500

    new app folderwith spacecats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spacecats::Application.config.secret_key_base = 'db682193d8fa9365f17568afccfb64056accb4bcb6ad9b9090e1c9c6e1c73ed471bc7a746c9263fc61eb6a019e7b6497c635241b502a872a3319fa76b363ccb4'
